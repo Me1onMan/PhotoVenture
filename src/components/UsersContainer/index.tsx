006d268abac8d5f2dbffce773487f0e34b00bb0f
Fyodor Shklyar <30theflame05@gmail.com>
2024-03-27 13:31:40 +0300

    feature: add listener for users

--- src/components/UsersContainer/index.tsx
+++ src/components/UsersContainer/index.tsx
@@ -1,33 +1,10 @@
-import { useEffect, useState } from 'react';
+import useUsers from '@/hooks/useUsers';
 
-import getUsers from '@/firebase/actions/getUsers';
-
-import { TUserCardProps } from './UserCard/types';
 import { UsersStyledContainer } from './styled';
 import UserCard from './UserCard';
 
 const UsersContianer = () => {
-  const [users, setUsers] = useState<Array<TUserCardProps>>([]);
-  const [isLoading, setIsLoading] = useState<boolean>(false);
-
-  useEffect(() => {
-    const loadUsers = async () => {
-      try {
-        setIsLoading(true);
-        const loadedUsers = await getUsers();
-        setUsers(loadedUsers);
-      } catch (error) {
-        console.log(error);
-        throw new Error('Error occured in UsersContainer useEffect()');
-      } finally {
-        setIsLoading(false);
-      }
-    };
-
-    loadUsers();
-  }, []);
-
-  if (isLoading) return <h1>Loading groups...</h1>;
+  const users = useUsers();
 
   return (
     <UsersStyledContainer>
