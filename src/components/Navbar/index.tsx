5d55f4a262b002f3f5c1ed3bf72dff4779023288
Fyodor Shklyar <30theflame05@gmail.com>
2024-03-29 16:43:12 +0300

    feature: move logout button to navbar

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,10 +1,22 @@
+import { useDispatch } from 'react-redux';
 import { NavLink } from 'react-router-dom';
 
 import navItems from '@/constants/navItems';
+import { logout } from '@/firebase';
+import { removeActiveUser } from '@/store/slices/activeUserSlice';
+
+import Button from '../UI/Button';
 
 import { Nav, NavItem, NavList } from './styled';
 
 const Navbar = () => {
+  const dispatch = useDispatch();
+
+  const handleLogout = async () => {
+    await logout();
+    dispatch(removeActiveUser());
+  };
+
   return (
     <Nav>
       <NavList>
@@ -24,6 +36,9 @@ const Navbar = () => {
           ))}
         </NavItem>
       </NavList>
+      <Button type="button" onClick={handleLogout}>
+        Logout
+      </Button>
     </Nav>
   );
 };
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,29 +1,13 @@
 import { FC } from 'react';
-import { useDispatch } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
 
 import AddPostForm from '@/components/AddPostForm';
 import Navbar from '@/components/Navbar';
 import PostsContainer from '@/components/PostsContainer';
-import Button from '@/components/UI/Button';
-import { logout } from '@/firebase';
-import { LOGIN_PAGE_ROUTE } from '@/router/routes';
-import { removeActiveUser } from '@/store/slices/activeUserSlice';
 
 const HomePage: FC = () => {
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
-
-  const handleLogout = async () => {
-    await logout();
-    dispatch(removeActiveUser());
-    navigate(LOGIN_PAGE_ROUTE, { replace: true });
-  };
-
   return (
     <div>
       <Navbar />
-      <Button onClick={handleLogout}>Logout</Button>
       <AddPostForm />
       <PostsContainer />
     </div>
