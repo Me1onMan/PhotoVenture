91a1424e6c2478df605041aad0717943a5a28782
Fyodor Shklyar <30theflame05@gmail.com>
2024-05-07 14:26:25 +0300

    feature: add filters to map page

--- src/components/Map/index.tsx
+++ src/components/Map/index.tsx
@@ -1,7 +1,14 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
 import mapboxgl from 'mapbox-gl/dist/mapbox-gl';
 
 import usePosts from '@/hooks/usePosts';
+import { selectFilterOptions } from '@/store/slices/filterOptionsSlice';
+import searchPosts from '@/utils/searchPosts';
+
+import { TPostProps } from '../PostsContainer/Post/types';
+import PostsFilterControl from '../UI/PostsFilterControl';
+import Search from '../UI/Search';
 
 import { MapContainer } from './styled';
 
@@ -9,6 +16,25 @@ const MAP_CONTAINER_ID = 'map-container';
 
 const Map = () => {
   const posts = usePosts();
+
+  const [searchValue, setSearchValue] = useState<string>('');
+  const [filteredPosts, setFilteredPosts] = useState<TPostProps[]>(posts);
+  const { types, emotions, access } = useSelector(selectFilterOptions);
+
+  useEffect(() => {
+    let tempPosts = posts;
+    tempPosts = tempPosts.filter((post) => searchPosts(post.data, searchValue));
+    if (types.length > 0) {
+      tempPosts = tempPosts.filter((post) => types.includes(post.data.postType));
+    }
+    if (emotions.length > 0) {
+      tempPosts = tempPosts.filter((post) => emotions.includes(post.data.emotion));
+    }
+    if (access.length > 0) {
+      tempPosts = tempPosts.filter((post) => access.includes(post.data.access));
+    }
+    setFilteredPosts(tempPosts);
+  }, [posts, searchValue, types, emotions, access]);
 
   useEffect(() => {
     mapboxgl.accessToken = import.meta.env.VITE_MAPBOX_ACCESS_TOKEN;
@@ -21,7 +47,7 @@ const Map = () => {
     map.addControl(new mapboxgl.NavigationControl());
 
     const addMapMarkers = () => {
-      posts.forEach((post) => {
+      filteredPosts.forEach((post) => {
         const popup = new mapboxgl.Popup({ offset: 25 }).setHTML(
           `<a href=/post/${post.id}>link</a>`,
         );
@@ -31,9 +57,20 @@ const Map = () => {
     };
 
     addMapMarkers();
-  }, [posts]);
+  }, [filteredPosts]);
 
-  return <MapContainer id={MAP_CONTAINER_ID} />;
+  return (
+    <>
+      <Search
+        value={searchValue}
+        setValue={setSearchValue}
+        name="postsSearch"
+        placeholder="Search in posts"
+      />
+      <PostsFilterControl />
+      <MapContainer id={MAP_CONTAINER_ID} />
+    </>
+  );
 };
 
 export default Map;
