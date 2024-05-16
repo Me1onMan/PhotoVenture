e81583b49ec3e8914f40b7508df27e3a7d8a64c5
Fyodor Shklyar <30theflame05@gmail.com>
2024-05-16 17:08:06 +0300

    fix: remove addButton on LikesPage

--- src/pages/LikesPage.tsx
+++ src/pages/LikesPage.tsx
@@ -1,39 +1,19 @@
-import { FC, useState } from 'react';
-import { createPortal } from 'react-dom';
+import { FC } from 'react';
 import { useSelector } from 'react-redux';
 
-import AddPostForm from '@/components/AddPostForm';
 import Navbar from '@/components/Navbar';
 import PostsContainer from '@/components/PostsContainer';
-import CreateButton from '@/components/UI/CreateButton';
-import withModalWrapper from '@/HOCs/ModalWrapper';
 import useLikedPosts from '@/hooks/useLikedPosts';
 import { selectActiveUser } from '@/store/slices/activeUserSlice';
-
-const MODAL = document.getElementById('modal');
 
 const LikesPage: FC = () => {
   const { id: userId } = useSelector(selectActiveUser);
   const likedPosts = useLikedPosts(userId);
 
-  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
-
-  const openModal = () => {
-    setIsModalOpen(true);
-  };
-
-  const closeModal = () => {
-    setIsModalOpen(false);
-  };
-
-  const AddModal = withModalWrapper(closeModal, AddPostForm);
-
   return (
     <>
       <Navbar />
-      <CreateButton onClick={openModal}>+</CreateButton>
       <PostsContainer posts={likedPosts} />
-      {isModalOpen && createPortal(<AddModal />, MODAL)}
     </>
   );
 };
